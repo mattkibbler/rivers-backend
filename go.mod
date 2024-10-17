f7539c549ec0fce8c10684266639c4a45fe2cfb6
Matt Kibbler <mattkibbler@gmail.com>
2024-10-17 22:54:45 +0100

    Generate zLevel of tiles using perlin noise generator

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mattkibbler/rivers-backend
 
 go 1.22.2
 
-require github.com/mattn/go-sqlite3 v1.14.24 // indirect
+require (
+	github.com/aquilax/go-perlin v1.1.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+)
--- services/tiles/generator.go
+++ services/tiles/generator.go
@@ -1,13 +1,40 @@
 package tiles
 
-import "math/rand"
+import (
+	"github.com/aquilax/go-perlin"
+)
+
+var p *perlin.Perlin
+
+func init() {
+	p = newPerlinNoiseGenerator()
+}
+
+func newPerlinNoiseGenerator() *perlin.Perlin {
+	// Set up the Perlin noise generator
+	alpha := 5.0      // The 'persistence' value
+	beta := 2.0       // The 'frequency' value
+	n := int32(3)     // Number of octaves (noise layers)
+	seed := int64(42) // Seed for randomness
+
+	return perlin.NewPerlin(alpha, beta, n, seed)
+}
+
+// Convert Perlin noise value (-1 to 1) to a height (0 to 256) as an integer
+func perlinNoiseToHeight(noise float64) int {
+	return int((noise + 1) / 2 * 256)
+}
 
 func GenerateTile(x int, y int) TileContent {
+	// Add a scale factor to vary the input to the Perlin noise function
+	scale := 0.06 // Adjust this value to control the "zoom level" of the noise
 	// Pick a random index from the slice
-	material := TileMaterials[rand.Intn(len(TileMaterials))]
+	//material := TileMaterials[rand.Intn(len(TileMaterials))]
+	material := TileMaterialStone
+	noiseValue := p.Noise2D(float64(x)*scale, float64(y)*scale)
 
 	return TileContent{
 		Material: material,
-		ZLevel:   1,
+		ZLevel:   perlinNoiseToHeight(noiseValue),
 	}
 }
